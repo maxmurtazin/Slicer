d52ea9eaa903028a08167383e3f1f46fec91deec
Murtazin Maxim <murtazinmaxim@outlook.com>
2023-12-01 20:30:14 +0300

    stepper mqt up and sown

--- Arduino/stepper.ino
+++ Arduino/stepper.ino
@@ -17,9 +17,14 @@ const int MV005 = 10  ;
 const int MV001 = 2 ;
 
 
-//MOVE(MV50, LOW)
+//MOVE(MV10, LOW)
+//MOVE(MV10, HIGH)
 
-//MOVE(MV50, HIGH)
+//MOVE(MV1, LOW)
+//MOVE(MV1, HIGH)
+
+//MOVE(MV01, LOW)
+//MOVE(MV01, HIGH)
 
 
 // ST 1/16
@@ -51,7 +56,20 @@ EthernetClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 // You don't need to change anything below this line!
-#define halt(s) { Serial.println(F( s )); while(1);  }
+//define halt(s) { Serial.println(F( s )); while(1);  }
+
+/****************************** Feeds ***************************************/
+
+
+
+//Adafruit_MQTT_Subscribe stepper_up = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/printer/stepper_up");
+//Adafruit_MQTT_Subscribe stepper_down = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/printer/stepper_down");
+
+
+Adafruit_MQTT_Subscribe STEPPER_UPP = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/printer/STEPPER_UPP");
+Adafruit_MQTT_Subscribe stepper_up = Adafruit_MQTT_Subscribe(&mqtt, STEPPER_UPP);
+Adafruit_MQTT_Subscribe STEPPER_DOWNN = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/printer/STEPPER_DOWNN");
+Adafruit_MQTT_Subscribe stepper_down = Adafruit_MQTT_Subscribe(&mqtt, STEPPER_DOWNN);
 
 
 void setup(){
@@ -73,14 +91,22 @@ void setup(){
      Serial.begin(115200);
      Ethernet.begin(mac);
   delay(1000); //give the ethernet a second to initialize
+   mqtt.subscribe(&stepper_down);
+   mqtt.subscribe(&stepper_up);
+
+
+     // ping the server to keep the mqtt connection alive
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
 
   }
 
 
-int MOVE (int heihgt, bool UPP){ //
+int MOVE_UP (int heihgt){ //
    // Установка направления вращения двигателя по часовой стрелке.
  
-  digitalWrite(dirPin, UPP); // по часовой 
+  digitalWrite(dirPin, HIGH); // по часовой
   // Медленное вращение двигателя
   for(int x = 0; x < heihgt; x++)
   {
@@ -92,10 +118,62 @@ int MOVE (int heihgt, bool UPP){ //
  // delay(5000); // Ждем 
 }
 
+int MOVE_DOWN (int heihgt){ //
+   // Установка направления вращения двигателя по часовой стрелке.
+
+  digitalWrite(dirPin, LOW); // по часовой
+  // Медленное вращение двигателя
+  for(int x = 0; x < heihgt; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+ // delay(5000); // Ждем
+}
+
 
 void loop(){
 MQTT_connect();
 
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+
+    if (subscription == &stepper_down) {
+      Serial.print(F("stepper down: "));
+      Serial.println((char *)stepper_down.lastread);
+
+      if (strcmp((char *)stepper_down.lastread, "10") == 0) {
+        MOVE_DOWN(MV10);
+      }
+      if (strcmp((char *)stepper_down.lastread, "1") == 0) {
+        MOVE_DOWN(MV1);
+      }
+      if (strcmp((char *)stepper_down.lastread, "01") == 0) {
+        MOVE_DOWN(MV01);
+      }
+
+    }
+
+
+       if (subscription == &stepper_up) {
+      Serial.print(F("stepper up: "));
+      Serial.println((char *)stepper_up.lastread);
+
+      if (strcmp((char *)stepper_up.lastread, "10") == 0) {
+        MOVE_UP(MV10);
+      }
+      if (strcmp((char *)stepper_up.lastread, "1") == 0) {
+        MOVE_UP(MV1);
+      }
+      if (strcmp((char *)stepper_up.lastread, "01") == 0) {
+        MOVE_UP(MV01);
+      }
+
+    }
+
+  delay(1000);
 }
 
 
